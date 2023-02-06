bde0f4f2fb961674011f2f547c5ca44747e6bb1f
himanshu pandey <himanshu.pandey7070@yahoo.com>
2023-02-06 13:10:58 +0530

    Delete config.js

--- server/config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  JWT_SECRET: '#7YXMU4JpmEnPlD8dKh3y$G%@',
-  EMAIL: 'mckenzie51@ethereal.email',
-  PASSWORD: 's85raxYCZJk4nW6NbS',
-  ATLAS_URI:
-    'mongodb+srv://admin:admin123@cluster0.y9slwx9.mongodb.net/?retryWrites=true&w=majority',
-};
